12e002800075e82c9d068ac6f881b832483460e3
Emiliano Massari <158219820+epsyFRG@users.noreply.github.com>
2025-09-10 14:01:51 +0200

    correction

--- src/Esercizio1Main.java
+++ src/Esercizio1Main.java
@@ -3,10 +3,10 @@ public class Esercizio1Main {
         Rettangolo r1 = new Rettangolo(5, 10);
         Rettangolo r2 = new Rettangolo(3, 7);
 
-        // Stampa singolo rettangolo
+        
         RettangoloUtils.stampaRettangolo(r1);
 
-        // Stampa due rettangoli e le somme
+        
         RettangoloUtils.stampaDueRettangoli(r1, r2);
     }
 }
